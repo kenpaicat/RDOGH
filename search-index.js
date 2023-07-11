8a20d25909b1040c921f0fdbf713f455d552211b
kenpaicat <kenpaicat@users.noreply.github.com>
2023-07-11 11:19:13 +0000

    deploy: 03a38de1ee538be5f40fe062280b6bcd76506440

---
+++ search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = JSON.parse('{\
+"RDOGH":{"doc":"","t":"F","n":["main"],"q":[[0,"RDOGH"]],"d":["Entry point of server."],"i":[0],"f":[[[]]],"c":[],"p":[]}\
+}');
+if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
+if (typeof exports !== 'undefined') {exports.searchIndex = searchIndex};
